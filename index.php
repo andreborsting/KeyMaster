4c4e8d196e79ddbeb015b6081c2269e5bad52c2d
AndreBorsting <andreborsting@gmail.com>
2025-01-29 16:45:00 +0100

    Eksperiment1

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
 </head>
 <body>
     <h1>KeyMaster</h1>
+<p>Work?</p>
+    <div class="game-container">
+        <div
+            class="player"
+            style="top:150px; left 150px;"
+        >
+            <div class="icon"></div>
+    </div>
+
+
+
+    </div>
 </body>
 </html>
