89eebc01a9988c55a478916044dfb4fb57c65b7e
AndreBorsting <andreborsting@gmail.com>
2025-02-10 14:06:05 +0100

    refactoring

--- create-game.php
+++ create-game.php
@@ -1,34 +1,12 @@
 <?php
-    $token = '11|RsmzCDzmZ8yRFXov1cznFZtDINrWUJwbcZkVU10o173dd83b';
+
+    require_once __DIR__ . '/api.php';
+
     $url = 'https://highscores.martindilling.com/api/v1/games';
-    $headers = [
-        'Accept: application/json',
-        'Content-type: application/json',
-        'Authorization: Bearer ' . $token,
-    ];
     $payload = [
-        'title' => 'ChooseYourPoints',
+        'title' => 'ChooseYourPoints'
     ];
-
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-        CURLOPT_URL => $url,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => json_encode($payload),
-        CURLOPT_HTTPHEADER => $headers,
-        CURLOPT_RETURNTRANSFER => true,
-    ]);
-    $responseData = curl_exec($curl);
-
-    // c/p -> Check if the response code indicates an error.
-    // 4xx and 5xx are client errors and server errors.
-    $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    if ($responseCode >= 400) {
-        echo 'HTTP Error: ' . $responseCode;
-    }
-    curl_close($curl);
-
-    $responseJson = json_decode($responseData);
+    $response = apiPost($url, $payload);
 
     echo '<pre>';
     echo json_encode($responseJson, JSON_PRETTY_PRINT);
--- delete-game.php
+++ delete-game.php
@@ -1,31 +1,9 @@
 <?php
-    $token = '11|RsmzCDzmZ8yRFXov1cznFZtDINrWUJwbcZkVU10o173dd83b';
-    $url = 'https://highscores.martindilling.com/api/v1/games';
-    $headers = [
-        'Accept: application/json',
-        'Content-type: application/json',
-        'Authorization: Bearer ' . $token,
-    ];
+    
+    require_once __DIR__ . '/api.php';
 
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-        CURLOPT_URL => $url,
-        CURLOPT_CUSTOMREQUEST => 'Delete',
-        CURLOPT_HTTPHEADER => $headers,
-        CURLOPT_RETURNTRANSFER => true,
-    ]);
-    $responseData = curl_exec($curl);
-
-
-    // c/p -> Check if the response code indicates an error.
-    // 4xx and 5xx are client errors and server errors.
-    $responseCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-    if ($responseCode >= 400) {
-        echo 'HTTP Error: ' . $responseCode;
-    }
-    curl_close($curl);
-
-    $responseJson = json_decode($responseData);
+    $url = 'https://highscores.martindilling.com/api/v1/games/11';
+    $response = apiDelete($url);
 
     echo '<pre>';
     echo json_encode($responseJson, JSON_PRETTY_PRINT);
