36d1761af5d7794c6da3e96bb3a0bf6dd1110bf3
AndreBorsting <andreborsting@gmail.com>
2025-02-14 09:14:54 +0100

    fixing small but critical mistakes

--- create-game.php
+++ create-game.php
@@ -1,15 +1,12 @@
 <?php
-
     require_once __DIR__ . '/api.php';
 
     $url = 'https://highscores.martindilling.com/api/v1/games';
     $payload = [
         'title' => 'ChooseYourPoints'
     ];
-    $response = apiPost($url, $payload);
+    $responseJson = apiPost($url, $payload);
 
     echo '<pre>';
     echo json_encode($responseJson, JSON_PRETTY_PRINT);
     echo '</pre>';
-
-?>
--- delete-game.php
+++ delete-game.php
@@ -2,11 +2,9 @@
     
     require_once __DIR__ . '/api.php';
 
-    $url = 'https://highscores.martindilling.com/api/v1/games/11';
+    $url = 'https://highscores.martindilling.com/api/v1/games/21';
     $response = apiDelete($url);
 
     echo '<pre>';
-    echo json_encode($responseJson, JSON_PRETTY_PRINT);
+    echo json_encode($response, JSON_PRETTY_PRINT);
     echo '</pre>';
-
-?>
